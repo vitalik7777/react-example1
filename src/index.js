bf5ad5a87f6620b32e73d074855814063a3f12c2
Vitalii Tyshkevych <v.tyshkevych@ism-ukraine.com>
2017-12-01 16:33:26 +0200

    vet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log(3);
+import React from 'react';
+import { render } from 'react-dom';
+
+class Square extends React.Component {
+    render() {
+        console.log(1);
+        return (
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+            {this.props.value}
+    </button>
+    );
+    }
+}
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+    render() {
+        return (
+            <div>
+            <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+    </div>
+        <div className="board-row">
+            {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+    </div>
+        <div className="board-row">
+            {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+    </div>
+        </div>
+        )
+    }
+}
+render (
+    <Board/>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+const path  = require('path')
 var webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: path.join(__dirname, 'src', 'index.js'),
     output: {
         path: __dirname + "/public/",
-        filename: "scripts.min.js"
-    }
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
 };
